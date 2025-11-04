b0c2c75a33512653b39c434c5ebf4cff3ff32d99
Dylan Rogers <opendylan@proton.me>
2025-11-04 12:05:44 +0000

    create Color::BLUE

--- src/color.rs
+++ src/color.rs
@@ -19,4 +19,5 @@ impl Color {
     pub const WHITE: Color = Color::rgb(255, 255, 255);
     pub const RED: Color = Color::rgb(255, 0, 0);
     pub const GREEN: Color = Color::rgb(0, 255, 0);
+    pub const BLUE: Color = Color::rgb(0, 0, 255);
 }
