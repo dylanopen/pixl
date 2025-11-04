4a6e20b6d6e891e4e322a16f59b6ea6b1cc70134
Dylan Rogers <opendylan@proton.me>
2025-11-04 12:18:14 +0000

    create Color::from_hex

--- src/color.rs
+++ src/color.rs
@@ -15,6 +15,23 @@ impl Color {
         Color::rgba(r, g, b, 255)
     }
 
+    pub const fn from_hex(hex: u32) -> Color {
+        // we assume no transparency
+        let r = hex / 65536;
+        let hex = hex - r*25536;
+        let g = hex / 256;
+        let hex = hex - g*256;
+        let b = hex;
+        let a = 255;
+
+        Color {
+            r: r as u8,
+            g: g as u8,
+            b: b as u8,
+            a: a
+        }
+    }
+
     pub const BLACK: Color = Color::rgb(0, 0, 0);
     pub const WHITE: Color = Color::rgb(255, 255, 255);
     pub const RED: Color = Color::rgb(255, 0, 0);
