1ba7a6afb5aee8afc4fc82d00c601c95ccd922df
Dylan Rogers <opendylan@proton.me>
2025-11-06 12:46:51 +0000

    create NodeSize

--- src/node.rs
+++ src/node.rs
@@ -20,3 +20,18 @@ pub trait NodePosition {
     }
 }
 
+pub trait NodeSize {
+    fn get_width(&self) -> usize;
+    fn get_height(&self) -> usize;
+    fn set_width(&mut self, width: usize);
+    fn set_height(&mut self, height: usize);
+
+    fn set_size(&mut self, width: usize, height: usize) {
+        self.set_width(width);
+        self.set_height(height);
+    }
+    fn get_size(&self) -> (usize, usize) {
+        (self.get_width(), self.get_height())
+    }
+}
+
