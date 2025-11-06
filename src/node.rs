b1febf7d8a23ea67adf5c28fbc479303c25750d4
Dylan Rogers <opendylan@proton.me>
2025-11-06 12:46:46 +0000

    create NodePosition

--- src/node.rs
+++ src/node.rs
@@ -5,3 +5,18 @@ pub trait NodeDraw {
     fn draw(&self, texture: &mut Texture);
 }
 
+pub trait NodePosition {
+    fn get_x(&self) -> usize;
+    fn get_y(&self) -> usize;
+    fn set_x(&mut self, x: usize);
+    fn set_y(&mut self, y: usize);
+
+    fn set_position(&mut self, x: usize, y: usize) {
+        self.set_x(x);
+        self.set_y(y);
+    }
+    fn get_position(&self) -> (usize, usize) {
+        (self.get_x(), self.get_y())
+    }
+}
+
