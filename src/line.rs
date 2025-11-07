d152bcd4368e02406c8bfe06532628526fab329f
Dylan Rogers <opendylan@proton.me>
2025-11-07 19:51:07 +0000

    create LineNode

--- src/line.rs
+++ src/line.rs
@@ -1,3 +1,30 @@
 //! `LineNode` struct - represents a node for a rectangle shape in a
 //! texture.
 
+use crate::Color;
+
+/// A node representing a line shape in a texture.
+/// Implemented components:
+/// - `DrawComponent`
+/// - `StrokeColorComponent`
+/// - `StrokeWidthComponent`
+#[non_exhaustive]
+#[expect(clippy::module_name_repetitions, reason = "struct should be called 'LineNode' as it is standard.")]
+pub struct LineNode {
+    /// The x-coordinate of the start point of the line.
+    pub x1: usize,
+
+    /// The y-coordinate of the start point of the line.
+    pub y1: usize,
+
+    /// The x-coordinate of the end point of the line.
+    pub x2: usize,
+
+    /// The y-coordinate of the end point of the line.
+    pub y2: usize,
+
+    /// The color of the line.
+    pub color: Color,
+}
+
+
