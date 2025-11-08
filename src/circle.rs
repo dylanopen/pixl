e6ca99c5515320ef75da6ca60bd7944c046e6e80
Dylan Rogers <opendylan@proton.me>
2025-11-08 13:04:53 +0000

    create CircleNode::new

--- src/circle.rs
+++ src/circle.rs
@@ -1,7 +1,7 @@
 //! The `CircleNode` struct, storing components to represent a filled circle on
 //! a texture.
 
-use crate::Color;
+use crate::{Color, component::DrawComponent};
 
 /// A node representing a circle shape to be drawn on a texture.
 /// It has a position (top left), size (width and height, must be equal and must
@@ -28,5 +28,22 @@ pub struct CircleNode {
 
     /// The color of the circle. It will be filled.
     pub fill_color: Color,
+}
+
+
+impl CircleNode {
+    /// Create a new `CircleNode` with the specified `x`, `y`, `radius` and
+    /// `fill_color`.
+    /// # Parameters:
+    /// - `x` - a usize containing the X coordinate of the center of the circle.
+    /// - `y` - a usize containing the Y coordinate of the center of the circle.
+    /// - `radius` - a usize containing the raidus of the circle.
+    /// - `fill_color` - a `Color` containing the color to fill in the circle.
+    /// # Returns
+    /// A `CircleNode` with the specified properties.
+    #[must_use]
+    pub const fn new(x: usize, y: usize, radius: usize, fill_color: Color) -> CircleNode {
+        CircleNode { x, y, radius, fill_color }
+    }
 }
 
