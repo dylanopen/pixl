4cbbfe989d991fa4d513d37709d0303b7e578e57
dylancode <opensource@dylancode.dev>
2026-06-02 12:00:04 +0100

    create VertexBuffer and create_vertex_buffer_from_raw

--- src/pixl.h
+++ src/pixl.h
@@ -25,7 +25,7 @@ RESULT_STRUCT(PixlWindow, PixlCreateWindowError);
 RESULT(PixlWindow, PixlCreateWindowError) pixl_create_window(uint32_t width, uint32_t height, char* title);
 
 
-//--- Colors, color.c ---/
+//--- Colors, color.c ---//
 
 typedef struct {
     uint8_t r;
@@ -36,6 +36,16 @@ typedef struct {
 
 PixlColor pixl_color_rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 PixlColor pixl_color_rgb(uint8_t r, uint8_t g, uint8_t b);
+
+
+//--- Vertex OpenGL storage, vertex.c ---//
+
+typedef struct {
+    GLuint vao;
+    GLuint vbo;
+} VertexBuffer;
+
+VertexBuffer pixl_create_vertex_buffer_from_raw(int number_of_vertices, int floats_per_vertex, float raw_vertex_data[]);
 
 
 #endif
---
+++ src/vertex.c
@@ -0,0 +1,21 @@
+#include "pixl.h"
+
+VertexBuffer pixl_create_vertex_buffer_from_raw(int number_of_vertices, int floats_per_vertex, float raw_vertex_data[]) {
+    GLuint vbo = 0;
+    glad_glGenBuffers(1, &vbo);
+    glad_glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glad_glBufferData(GL_ARRAY_BUFFER, number_of_vertices * sizeof(float), raw_vertex_data, GL_STATIC_DRAW);
+
+    GLuint vao = 0;
+    glad_glGenVertexArrays(1, &vao);
+    glad_glBindVertexArray(vao);
+    glad_glEnableVertexAttribArray(0);
+    glad_glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glad_glVertexAttribPointer(0, floats_per_vertex, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    return (VertexBuffer) {
+	.vao = vao,
+	.vbo = vbo,
+    };
+}
+
