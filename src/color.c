bf4df4106e960147dad6e9bbe0e91d13d93bc3a2
dylancode <opensource@dylancode.dev>
2026-06-02 11:43:15 +0100

    create fn color_rgba

--- src/color.c
+++ src/color.c
@@ -1,2 +1,10 @@
 #include "pixl.h"
 
+PixlColor pixl_color_rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
+    return (PixlColor) {
+	.r = r,
+	.g = g,
+	.b = b,
+	.a = a,
+    };
+}
--- src/pixl.h
+++ src/pixl.h
@@ -34,5 +34,7 @@ typedef struct {
     uint8_t a;
 } PixlColor;
 
+PixlColor pixl_color_rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+
 
 #endif
